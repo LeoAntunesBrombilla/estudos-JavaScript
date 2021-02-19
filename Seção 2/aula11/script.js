86f82a5350927e3f8f02dbb981ad62ac62a63b34
Leonardo Brombilla Antunes <76003107+LeoAntunesBrombilla@users.noreply.github.com>
2021-02-19 07:09:40 -0300

    Delete Seção 2 directory

--- Seção 2/aula11/script.js
+++
@@ -1,9 +0,0 @@
-let num1 = prompt("Digite um valor");
-let num2 = prompt("Digite outro número");
-
-num1 = Number(num1);
-num2 = Number(num2);
-
-const somatorio = num1 + num2;
-
-alert(`A soma dos valores é ${somatorio}`);
--- Seção 2/aula14/math.js
+++
@@ -1,6 +0,0 @@
-let num1 = 9.49;
-// let num2 = Math.floor(num1);
-// let num2 = Math.ceil(num1);
-
-let num2 = Math.round();
-console.log(num2);
