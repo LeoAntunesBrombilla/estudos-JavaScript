aa349e90f5b852a6a77a8c6368253557be66ae44
Leonardo Brombilla Antunes <76003107+LeoAntunesBrombilla@users.noreply.github.com>
2021-02-19 07:08:20 -0300

    Add files via upload

---
+++ Seção 2/aula19/valoresPrimitivosReferencia.js
@@ -0,0 +1,9 @@
+// Primitivos (imutáveis) - string, number, boolean, undefined, null, (bigint, symbol) - Valores copiados
+
+// Referemcia (mutável) - array, object, funciton - Passados por referência
+
+// para copiar por referencia
+
+let a = [1,2,3];
+let = [...a];
+let c = b;
